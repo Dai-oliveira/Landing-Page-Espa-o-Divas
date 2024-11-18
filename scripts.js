978c04cab785cf3e960b27b9d0a1a7d45cc80522
Daiani <daianirodrigues991@gmail.com>
2024-11-18 15:29:10 -0300

    primeiro commit

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-document.getElementById('meuFormulario').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const nome = document.getElementById('nome').value;
-    const procedimento = document.getElementById('procedimento').value;
+document.getElementById('meuFormulario').addEventListener('submit', function(event) 
+{ event.preventDefault(); 
+    
+const nome = document.getElementById('nome').value;
+const procedimento = document.getElementById('procedimento').value; 
+    
+let mensagem = `Parabéns ${nome}, Você acabou de ganhar um Bônus de 10% de DESCONTO (no procedimento escolhido). Se você recebeu este alerta tire um print entre em CONTATO e saiba como fazer para adquirir seu bônus!!!`;
 
-    let mensagem = `Bem vindo(a) ao Espaço Divas ${nome}, você ganhou 10% de desconto no procedimento escolhido.`;
-    if (!procedimento) {
-        mensagem += ' nenhum procedimento escolhido.';
-    }
-    alert(mensagem);
+if (!procedimento) { mensagem += ' nenhum procedimento escolhido.'; } 
+    
+alert(mensagem); 
 
-    // Limpar os campos do formulário após o envio
-    document.getElementById('nome').value = '';
-    document.getElementById('procedimento').value = '';
+document.getElementById('nome').value = '';
+document.getElementById('procedimento').value = '';
 });
